75ab6f00a5e4aaed948049e5774702f03d68f19f
Darren Bowers <darren.bowers@g.austincc.edu>
2021-04-07 15:20:18 -0500

    added several attempts as making planets_list function for review

--- functions.py
+++ functions.py
@@ -4,7 +4,68 @@
 # Purpose: Files use to identify true planets and which of them are closest and farthest from the sun.
 ######################
 
-def planets_lists()
+def planets_list(sol, status):
+# I tried a whole bunch of stuff but I recreated a streamlined version
+# of my throught process below
+
+
+# This is where I started - the way I understand this is it couldn't check if
+# status was true because the index of sol were keys instead of 0,1,2 etc. 
+    planets = [] #TypeError: list indices must be intergers or slices, not str
+    for key in sol:
+        if status[key] == True:
+            planets.append(sol[key])
+    return(planets)
+
+# So I made this instead, using that to cycle through status indexes to verify
+# if true. But I get the KeyError on line 15. Which if I understand correctly means
+# when its trying to append planets it's using the index of status and not finding
+# a corresponding element. I don't understand why though. 
+    planets = [] #KeyError: 0
+    for key in range(len(status)):
+        if status[key] == True:
+            planets.append(sol[key])
+    return(planets)
+
+# So I added an extra loops thinking this way it would grab the keys instead of 
+# an element associated with the index of status. But it just pulled the elements
+# over and over.
+    planets = [] # Grabs full list elements over and over
+    for i in range(len(status)):
+        if status[i] == True:
+            for key in sol:
+                planets.append(sol[key])
+    return(planets)
+
+# Since the last one was grabbing elements, I thought reducing it from (sol[key])
+# to (key) would resolve that, but it grabs the entire list of keys instead
+# of one at a time
+    planets = [] #Grabs all keys, over and over. 
+    for i in range(len(status)):
+        if status[i] == True:
+            for key in sol:
+                planets.append(key)
+    return(planets)
+
+# So I thought maybe its because the loop for sol in inside the loop of status
+# and I moved it around a bit but then that grabs multiples of the keys again
+    planets = []
+    for key in sol:
+        for i in range(len(status)):
+            if status[i] == True:
+                planets.append(key)
+    return(planets)
+
+# From there thats where I decided to pull the two loops apart.
+    full_list = [] #works but inefficent 
+    planets = []
+    for key in sol:
+        full_list.append(key)
+
+    for value in range(len(status)):
+        if status[value] == True:
+            planets.append(full_list[value])
+    return(planets)
 
 # write a function called planets_list()
 # pass it the planets dictionary and status list
