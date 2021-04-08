30b7571c42d97199867be515076bedfe68fac0d3
Darren Bowers <darren.bowers@g.austincc.edu>
2021-04-07 20:41:40 -0500

    Removed instructions

--- functions.py
+++ functions.py
@@ -13,14 +13,6 @@ def planets_list(sol, status):
             planets.append(key)
     return(planets)
 
-# write a function called planets_list()
-# pass it the planets dictionary and status list
-# return the list of planets that are true planets
-# base on the status list
-#
-# you may NOT use python methods; min, max, sum, or mean
-# or any "magic" functions. you must use a loop to do
-# this manually
 
 def furthest(sol, mixflag=True):
     furthest = 0
@@ -37,17 +29,6 @@ def furthest(sol, mixflag=True):
     return(furthest)    
 
 
-# write a function called furthest()
-# pass it the planets dictionary and an OPTIONAL boolean flag
-# the flag decides if you use the CLOSEST distance or the FURTHEST
-# distance to decide which planet is furthest
-# in other words, use index 0 or 1 from the distance lists
-# return the name of the furthest planet
-#
-# you may not use python methods; min, max, sum, or mean
-# or any "magic" functions. you must use a loop to do
-# this manually
-
 def closest(sol, mixflag=True):
     closest = 0
     distance = 10000
@@ -62,17 +43,6 @@ def closest(sol, mixflag=True):
                 closest = key
     return(closest)
 
-
-# write a function called closest()
-# pass it the planets dictionary and an OPTIONAL boolean flag
-# the flag decides if you use the CLOSEST distance or the FURTHEST
-# distance to decide which planet is closest
-# in other words, use index 0 or 1 from the distance lists
-# return the name of the closest planet
-#
-# you may not use python methods; min, max, sum, or mean
-# or any "magic" functions. you must use a loop to do
-# this manually
 
 
 
